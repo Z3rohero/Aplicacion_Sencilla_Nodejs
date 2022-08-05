f6446f97ff164ec7728819bc04fade2e0e73383c
z3rohero <109614528+Z3rohero@users.noreply.github.com>
2022-08-05 19:18:05 +0000

    xd

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,6 @@ router.get('/', (req, res) => res.render('index'))
 
 router.get('/about', (req, res) => res.render('about', { title: ' xDDDDDDDD' }))
 
-router.get('/contact', (req, res) => res.render('contact', { title: ' xDDDDDDDD' }))
+router.get('/contact', (req, res) => res.render('contact', { title: 'Contacto ' }))
 
 export default router
